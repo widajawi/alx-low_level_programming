6537c9b3ae9bbb9c721f26da52c4610c1666c920
widajawi <zakaria.elmajdoubi@uit.ac.ma>
2023-08-10 19:02:19 -0700

    print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char str1[70];
+
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+puts(str1);
+
+
+return (0);
+}
