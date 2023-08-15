d329f4adecf412c839b6d3d19c0cfef2404da8ae
widajawi <zakaria.elmajdoubi@uit.ac.ma>
2023-08-15 16:58:28 -0700

    hh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,8 @@
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+int main(void){
+printf("_putchar\n");
 
 	return (0);
 }
