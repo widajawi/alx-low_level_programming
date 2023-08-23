db4ad0825a2a4d77bdd3ec464d79aa4b312f050a
widajawi <zakaria.elmajdoubi@uit.ac.ma>
2023-08-23 11:03:41 -0700

    pointer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,8 @@
  */
 void print_number(int n)
 {
-	unsigned int num;
-/*check if number is negative*/
-	num = n;
-	if (n < 0)
-	{
-		_putchar(45);
-		num = -n;
-	}
-/* print number by recursion*/
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
+	unsigned int num; 
+
+
 	_putchar((num % 10) + '0');
 }
